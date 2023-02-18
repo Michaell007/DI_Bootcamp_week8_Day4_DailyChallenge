c9611d13ed7359d7af01519f04ae06ba1f500742
Michael Yapi .A <michaelakichi@gmail.com>
2023-02-18 17:56:25 +0000

    Update index.sql

--- index.sql
+++ index.sql
@@ -1,28 +1,33 @@
--- 1- Créez une procédure stockée simple nommée add()qui renvoie le produit de deux entiers à l'aide de plpgsql.
-CREATE OR REPLACE FUNCTION multiply(a integer, b integer)
-  RETURNS integer AS $$
-BEGIN
-  RETURN a * b;
-END;
-$$ LANGUAGE plpgsql;
+<?php
 
--- 2- Connectez-vous au serveur de base de données PostgreSQL en créant une nouvelle instance de la PDOclasse.
-$host = 'your_host';
-$dbname = 'your_database_name';
-$user = 'your_username';
-$password = 'your_password';
-$dsn = "pgsql:host=$host;dbname=$dbname;user=$user;password=$password";
-$pdo = new PDO($dsn);
+try {
+    $db = new PDO('pgsql:host=localhost;port=5432;dbname=daily', 'postgres', 12136270);
+} catch (PDOException $e) {
+    echo "Failed to connect to the database: " . $e->getMessage();
+}
 
--- 3- Préparez l'instruction qui appelle la procédure stockée pour exécution à l'aide de la prepare()méthode de l'objet PDO. La prepare()méthode renvoie un PDOStatementobjet.
-$sql = "SELECT multiply(:a, :b)";
-$stmt = $pdo->prepare($sql);
 
--- 4- Transmettez éventuellement des valeurs à l'instruction à l'aide de la bindValue()méthode.
-$stmt->bindValue(':a', 5, PDO::PARAM_INT);
-$stmt->bindValue(':b', 7, PDO::PARAM_INT);
+$db->query("
+    CREATE OR REPLACE FUNCTION add(
+        a INTEGER,
+        b INTEGER)
+      RETURNS integer AS $$
+    BEGIN
+        return a * b;
+    END; $$
+      LANGUAGE 'plpgsql';
+");
 
---5- Exécutez l'instruction à l'aide de la execute()méthode de l'objet PDOStatement
-$stmt->execute();
-$result = $stmt->fetchColumn();
-echo $result; // Output: 35
+$statement = $db->prepare("SELECT add(:a, :b)");
+
+$a = 20;
+$b = 100;
+$statement->bindValue(':a', $a, PDO::PARAM_INT);
+$statement->bindValue(':b', $b, PDO::PARAM_INT);
+
+$statement->execute();
+
+$result = $statement->fetchColumn();
+
+echo "The result of $a * $b is $result";
+?>
